8f11dc7774a6e9ac01861bdf547c1f869182549b
Noah Segal <n.h.segal@gmail.com>
2022-08-19 14:29:30 +0900

    add buttons

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const btns = document.querySelectorAll('button');
+const result = document.querySelector('#this-round');
+const score = document.querySelector('#score');
+const final = document.querySelector('#final-result');
+
+
+let yourScore = 0;
+let computerScore = 0;
+
+btns.forEach(btn => 
+    {
+        btn.addEventListener('click', ()=>displayResult(oneRound(btn.id, getComputerChoice())));
+    }
+)
+ 
 function getComputerChoice() {
     let moves = ['rock', 'paper', 'scissors'];
     let pick = Math.floor(Math.random()*3);
@@ -5,8 +20,6 @@ function getComputerChoice() {
 }
 
 function oneRound(playerSelection, computerSelection) {
-    
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == 'rock') {
         if (computerSelection == 'rock') {
             return 0;
@@ -46,6 +59,34 @@ function oneRound(playerSelection, computerSelection) {
 }
 
 
+function displayResult(v) {
+    if (v === 0) {
+        result.setAttribute('style', '-webkit-text-fill-color: yellow; -webkit-text-color: black;');
+        result.innerHTML = "Tie!";
+    }
+    else if (v === 1) {      
+        result.setAttribute('style', 'color: green;');
+        result.innerHTML = "You Win!";
+        yourScore++;
+    }
+    else if (v === -1) {
+        result.setAttribute('style', 'color: red;');
+        result.innerHTML = "You Lose!";
+        computerScore++;
+    }
+
+    score.innerHTML = `Score: ${yourScore} to ${computerScore}`;
+
+    if (yourScore >= 5) {
+        final.innerHTML = "You won the match!";
+        return;
+    }
+    else if (computerScore >=5) {
+        final.innerHTML = "You lost the match!"
+        return;
+    }
+    return;
+}
 
 
 
@@ -88,4 +129,6 @@ function game(){
     }
 }
 
-game();
+
+
+//game();
